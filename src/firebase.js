7b3df2a0de15a4c8ab3974d495a9bb1d24beb4a8
erne5t <ernestmatta@gmail.com>
2021-03-26 16:17:24 +0100

    new firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,8 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAZXXENK4SINV85fU8EyscYHwEdFgQnmPk",
-  authDomain: "wedodefi.firebaseapp.com",
-  projectId: "wedodefi",
-  storageBucket: "wedodefi.appspot.com",
-  messagingSenderId: "945080963099",
-  appId: "1:945080963099:web:996f28389459d050a7a8be",
-  measurementId: "G-K21Z9BTVHV"
+  apiKey: "AIzaSyDvnkr3GwO5ju0us_NyVauUhKaX5_WO85w",
+  authDomain: "react-clones-2699e.firebaseapp.com",
+  projectId: "react-clones-2699e",
+  storageBucket: "react-clones-2699e.appspot.com",
+  messagingSenderId: "410503656505",
+  appId: "1:410503656505:web:6527dea5a033c24a2789e8"
 };
